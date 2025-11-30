f692f6fbe6f215fcacf559f8275b674d51fabede
MaxTechnik <maxtechnik.oficial@gmail.com>
2025-11-30 18:42:24 +0100

    nefacha looting

--- src/main/java/cz/maxtechnik/dif/init/events/DifMod_ModForgeEvents.java
+++ src/main/java/cz/maxtechnik/dif/init/events/DifMod_ModForgeEvents.java
@@ -2,18 +2,18 @@ package cz.maxtechnik.dif.init.events;
 
 import cz.maxtechnik.dif.DifMod;
 import cz.maxtechnik.dif.init.basic.DifModItems;
-import net.minecraft.advancements.critereon.EnchantmentPredicate;
-import net.minecraft.advancements.critereon.ItemPredicate;
-import net.minecraft.advancements.critereon.MinMaxBounds;
+import net.minecraft.advancements.critereon.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ComposterBlock;
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
@@ -27,15 +27,18 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraft.world.level.storage.loot.predicates.LootItemEntityPropertyCondition;
 @Mod.EventBusSubscriber(modid=DifMod.MODID, bus=Mod.EventBusSubscriber.Bus.FORGE)
 public class DifMod_ModForgeEvents{
 	public static final ResourceLocation CHERRY_LEAVES_ID=ResourceLocation.fromNamespaceAndPath("minecraft","blocks/cherry_leaves");
 	public static final ResourceLocation CHISELED_BOOKSHELF_ID=ResourceLocation.fromNamespaceAndPath("minecraft","blocks/chiseled_bookshelf");
+	public static final ResourceLocation GOAT_ID=ResourceLocation.fromNamespaceAndPath("minecraft","entities/goat");
 	public static ItemStack emerald(int count){
 		return new ItemStack(Items.EMERALD,count);
 	}
 	@SubscribeEvent
 	public static void onLootTableLoad(LootTableLoadEvent event){
+		DifMod.LOGGER.debug(String.valueOf(event.getName()));
 		if(event.getName().equals(CHERRY_LEAVES_ID)){
 			LootItemCondition.Builder withoutSilkTouch=MatchTool.toolMatches(ItemPredicate.Builder.item().hasEnchantment(new EnchantmentPredicate(Enchantments.SILK_TOUCH,MinMaxBounds.Ints.atLeast(1)))).invert();
 			LootItemCondition.Builder withoutShears=MatchTool.toolMatches(ItemPredicate.Builder.item().of(Items.SHEARS)).invert();
@@ -54,6 +57,29 @@ public class DifMod_ModForgeEvents{
 					.setRolls(ConstantValue.exactly(1F))
 					.add(LootItem.lootTableItem(Blocks.CHISELED_BOOKSHELF.asItem()).apply(SetItemCountFunction.setCount(ConstantValue.exactly(1F)))).build();
 			table.addPool(pool);
+			event.setTable(table);
+		}else if(event.getName().equals(GOAT_ID)){
+			LootPool woolPool=LootPool.lootPool().name("dif_wool")
+					.setRolls(ConstantValue.exactly(1F))
+					.add(LootItem.lootTableItem(Blocks.WHITE_WOOL.asItem()).apply(SetItemCountFunction.setCount(ConstantValue.exactly(1F)))).build();
+			LootPool rawMeatPool=LootPool.lootPool().name("dif_raw_meat")
+					.setRolls(ConstantValue.exactly(1F))
+					.when(LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS,EntityPredicate.Builder.entity().flags(EntityFlagsPredicate.Builder.flags().setOnFire(false).build())))
+					.add(LootItem.lootTableItem(Items.MUTTON)
+							.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1F))))
+							.apply(ApplyBonusCount.addUniformBonusCount(Enchantments.MOB_LOOTING,3))
+					.build();
+			LootPool cookedMeatPool=LootPool.lootPool().name("dif_cooked_meat")
+					.setRolls(ConstantValue.exactly(1F))
+					.when(LootItemEntityPropertyCondition.hasProperties(LootContext.EntityTarget.THIS,EntityPredicate.Builder.entity().flags(EntityFlagsPredicate.Builder.flags().setOnFire(true).build())))
+					.add(LootItem.lootTableItem(Items.COOKED_MUTTON)
+							.apply(SetItemCountFunction.setCount(ConstantValue.exactly(1F))))
+							.apply(ApplyBonusCount.addUniformBonusCount(Enchantments.MOB_LOOTING,3))
+					.build();
+			LootTable table=LootTable.EMPTY;
+			table.addPool(woolPool);
+			table.addPool(rawMeatPool);
+			table.addPool(cookedMeatPool);
 			event.setTable(table);
 		}
 	}
