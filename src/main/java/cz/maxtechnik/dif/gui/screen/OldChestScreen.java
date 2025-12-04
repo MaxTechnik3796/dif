251e64368b750986c3a460b9c4cc1acb06a04b97
MaxTechnik <maxtechnik.oficial@gmail.com>
2025-12-04 21:29:10 +0100

    old chest part 5

--- src/main/java/cz/maxtechnik/dif/gui/screen/OldChestScreen.java
+++ src/main/java/cz/maxtechnik/dif/gui/screen/OldChestScreen.java
@@ -8,26 +8,48 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import org.jetbrains.annotations.NotNull;
-public class OldChestScreen extends AbstractContainerScreen<OldChestMenu>{
-	private static final ResourceLocation TEXTURE=ResourceLocation.fromNamespaceAndPath("minecraft","textures/gui/container/generic_54.png");
-	public OldChestScreen(OldChestMenu container,Inventory inventory,Component text){
-		super(container,inventory,text);
-		this.imageWidth=176;
-		this.imageHeight=222;
-		this.inventoryLabelY=this.imageHeight-94;
+
+public class OldChestScreen extends AbstractContainerScreen<OldChestMenu> {
+	private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/container/generic_54.png");
+
+	public OldChestScreen(OldChestMenu container, Inventory inventory, Component text) {
+		super(container, inventory, text);
+		// Získáme počet řádků z menu
+		int rows = container.getChestRows();
+
+		this.imageWidth = 176;
+		// Dynamický výpočet výšky: 114 (základ pro inv a okraje) + počet řádků * 18
+		this.imageHeight = 114 + rows * 18;
+		this.inventoryLabelY = this.imageHeight - 94;
 	}
+
 	@Override
-	public void render(@NotNull GuiGraphics guiGraphics,int mouseX,int mouseY,float partialTicks){
+	public void render(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
 		this.renderBackground(guiGraphics);
-		super.render(guiGraphics,mouseX,mouseY,partialTicks);
-		this.renderTooltip(guiGraphics,mouseX,mouseY);
+		super.render(guiGraphics, mouseX, mouseY, partialTicks);
+		this.renderTooltip(guiGraphics, mouseX, mouseY);
 	}
+
 	@Override
-	protected void renderBg(GuiGraphics guiGraphics,float partialTicks,int gx,int gy){
-		RenderSystem.setShaderColor(1.0F,1.0F,1.0F,1.0F);
-		RenderSystem.enableBlend();
-		RenderSystem.defaultBlendFunc();
-		guiGraphics.blit(TEXTURE,this.leftPos,this.topPos,0,0,this.imageWidth,this.imageHeight);
-		RenderSystem.disableBlend();
+	protected void renderBg(GuiGraphics guiGraphics, float partialTicks, int mouseX, int mouseY) {
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+
+		int x = (this.width - this.imageWidth) / 2;
+		int y = (this.height - this.imageHeight) / 2;
+		int rows = this.menu.getChestRows();
+
+		// 1. Vykreslení horního okraje (Hlavička) - 17 pixelů
+		guiGraphics.blit(TEXTURE, x, y, 0, 0, this.imageWidth, 17);
+
+		// 2. Vykreslení řádků beden
+		// Protože textura má omezený počet řádků, budeme opakovat první řádek textury pro každý řádek bedny.
+		// První řádek slotů v 'generic_54.png' začíná na Y=17 a má výšku 18.
+		for (int i = 0; i < rows; i++) {
+			guiGraphics.blit(TEXTURE, x, y + 17 + (i * 18), 0, 17, this.imageWidth, 18);
+		}
+
+		// 3. Vykreslení spodní části (Inventář hráče) - 96 pixelů
+		// Ve standardní textuře začíná inventář hráče na Y=126 (pro double chest).
+		guiGraphics.blit(TEXTURE, x, y + 17 + (rows * 18), 0, 126, this.imageWidth, 96);
 	}
 }
