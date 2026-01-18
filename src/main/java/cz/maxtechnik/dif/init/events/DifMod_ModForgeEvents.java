fd5e517645a236c7e095fd4c6b4e622233175e81
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-01-18 21:51:38 +0100

    composter fix

--- src/main/java/cz/maxtechnik/dif/init/events/DifMod_ModForgeEvents.java
+++ src/main/java/cz/maxtechnik/dif/init/events/DifMod_ModForgeEvents.java
@@ -44,11 +44,11 @@ public class DifMod_ModForgeEvents{
 	public static void addComposterItems(FMLCommonSetupEvent event){
 		ComposterBlock.COMPOSTABLES.put(Blocks.BAMBOO.asItem(),0.4F);
 		ComposterBlock.COMPOSTABLES.put(DifModItems.MATA.get(),0.9F);
-		ComposterBlock.COMPOSTABLES.put(DifModItems.MATA_PLANT.get(),0.88F);
+		ComposterBlock.COMPOSTABLES.put(DifModItems.MATA_PLANT.get(),0.8F);
 		ComposterBlock.COMPOSTABLES.put(DifModItems.MATY_BLOCK.get(),1F);
 		ComposterBlock.COMPOSTABLES.put(DifModItems.CANOLA_PLANT.get(),1F);
 		ComposterBlock.COMPOSTABLES.put(DifModItems.CANOLA_SEEDS.get(),1F);
-		ComposterBlock.COMPOSTABLES.put(DifModItems.CHERRY.get(),0.65F);
+		ComposterBlock.COMPOSTABLES.put(DifModItems.CHERRY.get(),0.6F);
 	}
 	@SubscribeEvent
 	public static void registerTrades(VillagerTradesEvent event){
