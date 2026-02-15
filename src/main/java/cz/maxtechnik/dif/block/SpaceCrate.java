a730e130659bb37f23128e7699d5b8d104a52d84
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-02-15 20:44:55 +0100

    Space Crate working 50%

---
+++ src/main/java/cz/maxtechnik/dif/block/SpaceCrate.java
@@ -0,0 +1,121 @@
+package cz.maxtechnik.dif.block;
+
+import javax.annotation.Nullable;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.Container;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.piglin.PiglinAi;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.entity.BarrelBlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
+@SuppressWarnings("deprecation")
+public class SpaceCrate extends BaseEntityBlock{
+	public static final DirectionProperty FACING = BlockStateProperties.FACING;
+	public static final BooleanProperty OPEN = BlockStateProperties.OPEN;
+
+	public SpaceCrate(BlockBehaviour.Properties p_49046_) {
+		super(p_49046_);
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(OPEN, Boolean.valueOf(false)));
+	}
+
+	public @NotNull InteractionResult use(@NotNull BlockState p_49069_,Level p_49070_,@NotNull BlockPos p_49071_,@NotNull Player p_49072_,@NotNull InteractionHand p_49073_,BlockHitResult p_49074_) {
+		if (p_49070_.isClientSide) {
+			return InteractionResult.SUCCESS;
+		} else {
+			BlockEntity blockentity = p_49070_.getBlockEntity(p_49071_);
+			if (blockentity instanceof BarrelBlockEntity) {
+				p_49072_.openMenu((BarrelBlockEntity)blockentity);
+				p_49072_.awardStat(Stats.OPEN_BARREL);
+				PiglinAi.angerNearbyPiglins(p_49072_, true);
+			}
+
+			return InteractionResult.CONSUME;
+		}
+	}
+
+	public void onRemove(BlockState p_49076_,@NotNull Level p_49077_,@NotNull BlockPos p_49078_,BlockState p_49079_,boolean p_49080_) {
+		if (!p_49076_.is(p_49079_.getBlock())) {
+			BlockEntity blockentity = p_49077_.getBlockEntity(p_49078_);
+			if (blockentity instanceof Container) {
+				Containers.dropContents(p_49077_, p_49078_, (Container)blockentity);
+				p_49077_.updateNeighbourForOutputSignal(p_49078_, this);
+			}
+
+			super.onRemove(p_49076_, p_49077_, p_49078_, p_49079_, p_49080_);
+		}
+	}
+
+	public void tick(@NotNull BlockState p_220758_,ServerLevel p_220759_,@NotNull BlockPos p_220760_,@NotNull RandomSource p_220761_) {
+		BlockEntity blockentity = p_220759_.getBlockEntity(p_220760_);
+		if (blockentity instanceof BarrelBlockEntity) {
+			((BarrelBlockEntity)blockentity).recheckOpen();
+		}
+
+	}
+
+	@Nullable
+	public BlockEntity newBlockEntity(@NotNull BlockPos p_152102_,@NotNull BlockState p_152103_) {
+		return new BarrelBlockEntity(p_152102_, p_152103_);
+	}
+
+	public RenderShape getRenderShape(@NotNull BlockState p_49090_) {
+		return RenderShape.MODEL;
+	}
+
+	public void setPlacedBy(@NotNull Level p_49052_,@NotNull BlockPos p_49053_,@NotNull BlockState p_49054_,@Nullable LivingEntity p_49055_,ItemStack p_49056_) {
+		if (p_49056_.hasCustomHoverName()) {
+			BlockEntity blockentity = p_49052_.getBlockEntity(p_49053_);
+			if (blockentity instanceof BarrelBlockEntity) {
+				((BarrelBlockEntity)blockentity).setCustomName(p_49056_.getHoverName());
+			}
+		}
+
+	}
+
+	public boolean hasAnalogOutputSignal(@NotNull BlockState p_49058_) {
+		return true;
+	}
+
+	public int getAnalogOutputSignal(@NotNull BlockState p_49065_,Level p_49066_,@NotNull BlockPos p_49067_) {
+		return AbstractContainerMenu.getRedstoneSignalFromBlockEntity(p_49066_.getBlockEntity(p_49067_));
+	}
+
+	public @NotNull BlockState rotate(BlockState p_49085_,Rotation p_49086_) {
+		return p_49085_.setValue(FACING, p_49086_.rotate(p_49085_.getValue(FACING)));
+	}
+
+	public @NotNull BlockState mirror(BlockState p_49082_,Mirror p_49083_) {
+		return p_49082_.rotate(p_49083_.getRotation(p_49082_.getValue(FACING)));
+	}
+
+	protected void createBlockStateDefinition(StateDefinition.Builder<net.minecraft.world.level.block.Block,BlockState> p_49088_) {
+		p_49088_.add(FACING, OPEN);
+	}
+
+	public BlockState getStateForPlacement(BlockPlaceContext p_49048_) {
+		return this.defaultBlockState().setValue(FACING, p_49048_.getNearestLookingDirection().getOpposite());
+	}
+}
