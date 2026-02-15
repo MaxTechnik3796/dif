7c8020d3d2e6a69f61b054477d06449a1f4ad5ac
Suriken222 <suriken222a@gmail.com>
2026-02-15 23:02:59 +0100

    gg

--- src/main/java/cz/maxtechnik/dif/init/events/ClientSkyHandler.java
+++ src/main/java/cz/maxtechnik/dif/init/events/ClientSkyHandler.java
@@ -1,29 +1,64 @@
 package cz.maxtechnik.dif.init.events;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterDimensionSpecialEffectsEvent;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = "dif", bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientSkyHandler {
 
-	@SubscribeEvent
-	public static void onRenderSky(RenderLevelStageEvent event) {
-		// Musíme kreslit až po obloze, abychom ji mohli přikrýt
-		if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_SKY) {
-			Minecraft mc = Minecraft.getInstance();
-			if (mc.level != null) {
-				String dimLocation = mc.level.dimension().location().toString();
+	// --- ČÁST PRO MOD BUS (Vypnutí mraků a vanilla oblohy) ---
+	@Mod.EventBusSubscriber(modid = "dif", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ModBusEvents {
+		@SubscribeEvent
+		public static void onRegisterEffects(RegisterDimensionSpecialEffectsEvent event) {
+			// Vytvoříme vesmírné efekty
+			DimensionSpecialEffects spaceEffects = new DimensionSpecialEffects(
+					Float.NaN, // cloudLevel: NaN = TOTÁLNÍ ABSENCE MRAKŮ
+					false,     // hasGround: vypne mlhu pod horizontem
+					DimensionSpecialEffects.SkyType.NONE, // skyType: NONE = vypne vanilla slunce/měsíc
+					false,
+					false
+			) {
+				@Override
+				public Vec3 getBrightnessDependentFogColor(Vec3 fogColor, float brightness) {
+					return Vec3.ZERO; // Černá mlha
+				}
 
-				if (dimLocation.equals("dif:orbit")) {
-					OrbitSkyRenderer.render(
-							mc.level,
-							event.getPartialTick(),
-							event.getPoseStack(),
-							event.getProjectionMatrix()
-					);
+				@Override
+				public boolean isFoggyAt(int x, int z) {
+					return false;
+				}
+			};
+
+			// Zaregistrujeme je pod ID, která jsme napsali do JSONů
+			event.register(ResourceLocation.fromNamespaceAndPath("dif", "orbit"), spaceEffects);
+			event.register(ResourceLocation.fromNamespaceAndPath("dif", "moon"), spaceEffects);
+		}
+	}
+
+	// --- ČÁST PRO FORGE BUS (Vykreslení tvé Země) ---
+	@Mod.EventBusSubscriber(modid = "dif", bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+	public static class ForgeBusEvents {
+		@SubscribeEvent
+		public static void onRenderSky(RenderLevelStageEvent event) {
+			// Kreslíme pouze ve fázi oblohy
+			if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_SKY) {
+				Minecraft mc = Minecraft.getInstance();
+				if (mc.level != null) {
+					String dim = mc.level.dimension().location().toString();
+
+					if (dim.equals("dif:orbit")) {
+						OrbitSkyRenderer.renderCustomSky(mc.level, event.getPartialTick(), event.getPoseStack(), "orbit");
+					}
+					else if (dim.equals("dif:moon")) {
+						OrbitSkyRenderer.renderCustomSky(mc.level, event.getPartialTick(), event.getPoseStack(), "moon");
+					}
 				}
 			}
 		}
--- src/main/java/cz/maxtechnik/dif/init/events/OrbitSkyRenderer.java
+++ src/main/java/cz/maxtechnik/dif/init/events/OrbitSkyRenderer.java
@@ -10,9 +10,8 @@ import org.joml.Matrix4f;
 
 public class OrbitSkyRenderer {
 	private static final ResourceLocation EARTH_TEXTURE = ResourceLocation.fromNamespaceAndPath("dif", "textures/environment/earth.png");
-	private static final ResourceLocation MOON_TEXTURE = ResourceLocation.fromNamespaceAndPath("dif", "textures/environment/moon.png");
 
-	public static void render(ClientLevel level, float partialTick, PoseStack poseStack, Matrix4f projectionMatrix) {
+	public static void renderCustomSky(ClientLevel level, float partialTick, PoseStack poseStack, String mode) {
 		RenderSystem.disableDepthTest();
 		RenderSystem.enableBlend();
 		RenderSystem.defaultBlendFunc();
@@ -21,67 +20,62 @@ public class OrbitSkyRenderer {
 		Tesselator tesselator = Tesselator.getInstance();
 		BufferBuilder bufferbuilder = tesselator.getBuilder();
 
-		// --- 1. VYČIŠTĚNÍ OBLOHY (Odstranění vanilla slunce) ---
-		// Vykreslíme černé stěny kolem hráče, které "přetřou" původní oblohu
-		RenderSystem.setShader(GameRenderer::getPositionColorShader);
-		bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
-		float f = 150.0F; // Velikost černé bariéry
-
-		for (int i = 0; i < 6; ++i) {
+		if (mode.equals("orbit")) {
+			// REŽIM ORBITA: Země pod nohama
 			poseStack.pushPose();
-			if (i == 1) poseStack.mulPose(Axis.XP.rotationDegrees(90.0F));
-			if (i == 2) poseStack.mulPose(Axis.XP.rotationDegrees(-90.0F));
-			if (i == 3) poseStack.mulPose(Axis.XP.rotationDegrees(180.0F));
-			if (i == 4) poseStack.mulPose(Axis.ZP.rotationDegrees(90.0F));
-			if (i == 5) poseStack.mulPose(Axis.ZP.rotationDegrees(-90.0F));
+			RenderSystem.setShader(GameRenderer::getPositionTexShader);
+			RenderSystem.setShaderTexture(0, EARTH_TEXTURE);
+			Matrix4f matrix = poseStack.last().pose();
+			float planetSize = 250.0F;
 
-			Matrix4f m = poseStack.last().pose();
-			bufferbuilder.vertex(m, -f, -f, -f).color(0, 0, 0, 255).endVertex();
-			bufferbuilder.vertex(m, f, -f, -f).color(0, 0, 0, 255).endVertex();
-			bufferbuilder.vertex(m, f, -f, f).color(0, 0, 0, 255).endVertex();
-			bufferbuilder.vertex(m, -f, -f, f).color(0, 0, 0, 255).endVertex();
+			bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+			bufferbuilder.vertex(matrix, -planetSize, -100.0F, planetSize).uv(0, 1).endVertex();
+			bufferbuilder.vertex(matrix, planetSize, -100.0F, planetSize).uv(1, 1).endVertex();
+			bufferbuilder.vertex(matrix, planetSize, -100.0F, -planetSize).uv(1, 0).endVertex();
+			bufferbuilder.vertex(matrix, -planetSize, -100.0F, -planetSize).uv(0, 0).endVertex();
+			tesselator.end();
 			poseStack.popPose();
 		}
-		tesselator.end();
+		else if (mode.equals("moon")) {
+			// --- 1. POHYBLIVÁ ČERNÁ MASKA (Zakrytí měsíce) ---
+			poseStack.pushPose();
+			// Synchronizace s měsícem (+ 0.5F otočí masku proti slunci)
+			float moonAngle = level.getTimeOfDay(partialTick) + 0.5F;
+			poseStack.mulPose(Axis.ZP.rotationDegrees(moonAngle * 360.0F));
 
-		// --- 2. MĚSÍC (Nahoře) ---
-		float skyAngle = level.getTimeOfDay(partialTick);
-		poseStack.pushPose();
-		poseStack.mulPose(Axis.XP.rotationDegrees(skyAngle * 360.0F));
+			RenderSystem.setShader(GameRenderer::getPositionColorShader);
+			bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
+			Matrix4f maskMatrix = poseStack.last().pose();
 
-		RenderSystem.setShader(GameRenderer::getPositionTexShader);
-		RenderSystem.setShaderTexture(0, MOON_TEXTURE);
+			float maskSize = 16.0F;
+			// Výška 98.0F (blíže k hráči než nebe)
+			bufferbuilder.vertex(maskMatrix, -maskSize, 98.0F, -maskSize).color(0, 0, 0, 255).endVertex();
+			bufferbuilder.vertex(maskMatrix, maskSize, 98.0F, -maskSize).color(0, 0, 0, 255).endVertex();
+			bufferbuilder.vertex(maskMatrix, maskSize, 98.0F, maskSize).color(0, 0, 0, 255).endVertex();
+			bufferbuilder.vertex(maskMatrix, -maskSize, 98.0F, maskSize).color(0, 0, 0, 255).endVertex();
+			tesselator.end();
+			poseStack.popPose();
 
-		Matrix4f moonMatrix = poseStack.last().pose();
-		int phase = level.getMoonPhase();
-		float x1 = (float)(phase % 4) / 4.0F;
-		float y1 = (float)(phase / 4) / 2.0F;
-		float x2 = x1 + 0.25F;
-		float y2 = y1 + 0.5F;
+			// --- 2. STATICKÁ ZEMĚ NA SEVERU ---
+			poseStack.pushPose();
+			RenderSystem.setShader(GameRenderer::getPositionTexShader);
+			RenderSystem.setShaderTexture(0, EARTH_TEXTURE);
 
-		float moonSize = 20.0F;
-		bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-		bufferbuilder.vertex(moonMatrix, -moonSize, 100.0F, -moonSize).uv(x2, y2).endVertex();
-		bufferbuilder.vertex(moonMatrix, moonSize, 100.0F, -moonSize).uv(x1, y2).endVertex();
-		bufferbuilder.vertex(moonMatrix, moonSize, 100.0F, moonSize).uv(x1, y1).endVertex();
-		bufferbuilder.vertex(moonMatrix, -moonSize, 100.0F, moonSize).uv(x2, y1).endVertex();
-		tesselator.end();
-		poseStack.popPose();
+			// Polohování: 15° na sever (osa Z) a -20° sklon (osa X)
+			poseStack.mulPose(Axis.ZP.rotationDegrees(10.0F));
+			poseStack.mulPose(Axis.XP.rotationDegrees(-15.0F));
 
-		// --- 3. PLANETA (Dole) ---
-		poseStack.pushPose();
-		RenderSystem.setShaderTexture(0, EARTH_TEXTURE);
-		Matrix4f earthMatrix = poseStack.last().pose();
+			Matrix4f earthMatrix = poseStack.last().pose();
+			float earthSize = 12.0F;
 
-		float planetSize = 200.0F;
-
-		bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-		bufferbuilder.vertex(earthMatrix, -planetSize, -100.0F, planetSize).uv(0, 1).endVertex();
-		bufferbuilder.vertex(earthMatrix, planetSize, -100.0F, planetSize).uv(1, 1).endVertex();
-		bufferbuilder.vertex(earthMatrix, planetSize, -100.0F, -planetSize).uv(1, 0).endVertex();
-		bufferbuilder.vertex(earthMatrix, -planetSize, -100.0F, -planetSize).uv(0, 0).endVertex();
-		tesselator.end();
-		poseStack.popPose();
+			bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+			bufferbuilder.vertex(earthMatrix, -earthSize, 100.0F, -earthSize).uv(0, 1).endVertex();
+			bufferbuilder.vertex(earthMatrix, earthSize, 100.0F, -earthSize).uv(1, 1).endVertex();
+			bufferbuilder.vertex(earthMatrix, earthSize, 100.0F, earthSize).uv(1, 0).endVertex();
+			bufferbuilder.vertex(earthMatrix, -earthSize, 100.0F, earthSize).uv(0, 0).endVertex();
+			tesselator.end();
+			poseStack.popPose();
+		}
 
 		RenderSystem.depthMask(true);
 		RenderSystem.disableBlend();
