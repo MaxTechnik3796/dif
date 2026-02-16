aca3acf9681cda2e968f112df35b48a6ee36b698
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-02-16 21:28:37 +0100

    Spaceship rotation fix

--- src/main/java/cz/maxtechnik/dif/block/Spaceship.java
+++ src/main/java/cz/maxtechnik/dif/block/Spaceship.java
@@ -17,6 +17,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
@@ -119,5 +120,15 @@ public class Spaceship extends Block implements EntityBlock{
 	@Override
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block,BlockState> builder){
 		builder.add(FACING);
+	}
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context){
+		return this.defaultBlockState().setValue(FACING,context.getHorizontalDirection().getOpposite());
+	}
+	public @NotNull BlockState rotate(BlockState state,Rotation rot){
+		return state.setValue(FACING,rot.rotate(state.getValue(FACING)));
+	}
+	public @NotNull BlockState mirror(BlockState state,Mirror mirrorIn){
+		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
 	}
 }
