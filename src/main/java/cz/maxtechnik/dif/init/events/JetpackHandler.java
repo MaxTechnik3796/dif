ebc932592f90544cfbc00924293d08f6709d9bb9
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-02-18 13:00:29 +0100

    hotfix

--- src/main/java/cz/maxtechnik/dif/init/events/JetpackHandler.java
+++ src/main/java/cz/maxtechnik/dif/init/events/JetpackHandler.java
@@ -6,7 +6,6 @@ import cz.maxtechnik.dif.init.basic.DifModItems;
 import cz.maxtechnik.dif.item.armor.Jetpack;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
