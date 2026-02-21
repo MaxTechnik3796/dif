e14cd7cf47200e192b7dd2c96f62e1d6b3f1036d
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-02-22 00:10:10 +0100

    OrbitVoidTeleport fix

--- src/main/java/cz/maxtechnik/dif/init/events/OrbitVoidTeleport.java
+++ src/main/java/cz/maxtechnik/dif/init/events/OrbitVoidTeleport.java
@@ -3,13 +3,18 @@ package cz.maxtechnik.dif.init.events;
 import cz.maxtechnik.dif.DifMod;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.HashSet;
+import java.util.Objects;
 @Mod.EventBusSubscriber
 public class OrbitVoidTeleport{
 	@SubscribeEvent
@@ -18,12 +23,13 @@ public class OrbitVoidTeleport{
 		ResourceLocation currentDim=entity.level().dimension().location();
 		if(currentDim.equals(ResourceLocation.fromNamespaceAndPath(DifMod.MODID,"orbit"))){
 			if(entity.getY()<=-90){
+				ServerLevel overworld=Objects.requireNonNull(entity.getServer()).getLevel(Level.OVERWORLD);
+				assert overworld!=null;
+				entity.teleportTo(overworld,entity.getX(),500,entity.getZ(),new HashSet<>(),entity.getYRot(),entity.getXRot());
 				entity.addEffect(new MobEffectInstance(MobEffects.BLINDNESS,100,0,true,false));
 				entity.setSecondsOnFire(10);
 				entity.fallDistance=0;
-				entity.teleportTo(entity.getX(),500,entity.getZ());
 				if(entity instanceof ServerPlayer serverPlayer)serverPlayer.displayClientMessage(Component.literal("§6You are falling back to Overworld!"),true);
-
 			}
 		}
 	}
