98e24dd343a5c31b955da76ba751610aefd8ea8d
Suriken222 <suriken222a@gmail.com>
2026-03-08 16:39:02 +0100

    enderchest on back

--- src/main/java/cz/maxtechnik/dif/init/other/DifModKeys.java
+++ src/main/java/cz/maxtechnik/dif/init/other/DifModKeys.java
@@ -3,6 +3,7 @@ package cz.maxtechnik.dif.init.other;
 import cz.maxtechnik.dif.DifMod;
 import com.mojang.blaze3d.platform.InputConstants;
 import cz.maxtechnik.dif.network.JetpackFlyMessage;
+import cz.maxtechnik.dif.network.EnderOpenMessage;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
@@ -11,21 +12,29 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
+
 @Mod.EventBusSubscriber(modid=DifMod.MODID, bus=Mod.EventBusSubscriber.Bus.MOD, value=Dist.CLIENT)
-public class DifModKeys{
-	public static final KeyMapping JETPACK_FLY=new KeyMapping("key.dif.jetpack_fly",InputConstants.Type.KEYSYM,GLFW.GLFW_KEY_SPACE,"key.categories.dif");
+public class DifModKeys {
+	public static final KeyMapping JETPACK_FLY = new KeyMapping("key.dif.jetpack_fly", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_SPACE, "key.categories.dif");
+	public static final KeyMapping OPEN_ENDER_CHEST = new KeyMapping("key.dif.open_ender", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_V, "key.categories.dif");
+
 	@SubscribeEvent
 	public static void registerKeys(RegisterKeyMappingsEvent event){
 		event.register(JETPACK_FLY);
+		event.register(OPEN_ENDER_CHEST);
 	}
+
 	@Mod.EventBusSubscriber(modid=DifMod.MODID, value=Dist.CLIENT, bus=Mod.EventBusSubscriber.Bus.FORGE)
-	public static class ClientTickHandler{
+	public static class ClientTickHandler {
 		@SubscribeEvent
 		public static void onClientTick(TickEvent.ClientTickEvent event){
-			if(event.phase==TickEvent.Phase.END&&Minecraft.getInstance().player!=null){
+			if(event.phase==TickEvent.Phase.END && Minecraft.getInstance().player!=null){
 				if(JETPACK_FLY.isDown()){
 					DifMod.PACKET_HANDLER.sendToServer(new JetpackFlyMessage(0,0));
 					JetpackFlyMessage.pressAction(Minecraft.getInstance().player,0);
+				}
+				while(OPEN_ENDER_CHEST.consumeClick()){
+					DifMod.PACKET_HANDLER.sendToServer(new EnderOpenMessage(0,0));
 				}
 			}
 		}
---
+++ src/main/java/cz/maxtechnik/dif/network/EnderOpenMessage.java
@@ -0,0 +1,78 @@
+package cz.maxtechnik.dif.network;
+
+import cz.maxtechnik.dif.DifMod;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.SimpleMenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.inventory.PlayerEnderChestContainer;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.network.NetworkEvent;
+import org.jetbrains.annotations.NotNull;
+import top.theillusivec4.curios.api.CuriosApi;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.inventory.MenuType;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+@SuppressWarnings("deprecation")
+@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+public class EnderOpenMessage {
+    int type, pressedms;
+
+    public EnderOpenMessage(int type, int pressedms) {
+        this.type = type;
+        this.pressedms = pressedms;
+    }
+
+    public EnderOpenMessage(FriendlyByteBuf buffer) {
+        this.type = buffer.readInt();
+        this.pressedms = buffer.readInt();
+    }
+
+    public static void buffer(EnderOpenMessage message, FriendlyByteBuf buffer) {
+        buffer.writeInt(message.type);
+        buffer.writeInt(message.pressedms);
+    }
+
+    public static void handler(EnderOpenMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+        NetworkEvent.Context context = contextSupplier.get();
+        context.enqueueWork(() -> pressAction(Objects.requireNonNull(context.getSender()), message.type));
+        context.setPacketHandled(true);
+    }
+
+	public static void pressAction(Player player, int type) {
+		Level world = player.level();
+		if (!world.hasChunkAt(player.blockPosition())) return;
+		if (type == 0 && player instanceof ServerPlayer serverPlayer) {
+			CuriosApi.getCuriosInventory(serverPlayer).ifPresent(handler ->handler.findFirstCurio(stack -> stack.getItem() == Items.ENDER_CHEST).ifPresent(slotResult -> {
+				PlayerEnderChestContainer enderChestInventory = serverPlayer.getEnderChestInventory();
+				SimpleMenuProvider menuProvider = new SimpleMenuProvider(
+						(containerId, playerInventory, playerEntity) ->new ChestMenu(MenuType.GENERIC_9x3, containerId, playerInventory, enderChestInventory, 3) {
+							@Override
+							public void removed(@NotNull Player pPlayer) {
+								super.removed(pPlayer);
+								pPlayer.level().playSound(null, pPlayer.blockPosition(), SoundEvents.ENDER_CHEST_CLOSE, SoundSource.PLAYERS, 0.5F, pPlayer.level().random.nextFloat() * 0.1F + 0.9F);
+							}
+						},
+						Component.translatable("container.enderchest")
+				);
+				serverPlayer.openMenu(menuProvider);
+				serverPlayer.level().playSound(null, serverPlayer.blockPosition(), SoundEvents.ENDER_CHEST_OPEN, SoundSource.PLAYERS, 0.5F, serverPlayer.level().random.nextFloat() * 0.1F + 0.9F);
+			}));
+		}
+	}
+
+    @SubscribeEvent
+    public static void registerMessage(FMLCommonSetupEvent event) {
+        DifMod.addNetworkMessage(EnderOpenMessage.class, EnderOpenMessage::buffer, EnderOpenMessage::new, EnderOpenMessage::handler);
+    }
+}
