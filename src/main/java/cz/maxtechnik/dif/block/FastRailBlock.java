0cb2822ef045b61dba158dc0f1ea747c37f91c67
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-04-02 10:44:23 +0200

    Fast rails speed fix

--- src/main/java/cz/maxtechnik/dif/block/FastRailBlock.java
+++ src/main/java/cz/maxtechnik/dif/block/FastRailBlock.java
@@ -5,16 +5,14 @@ import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.RailBlock;
 import net.minecraft.world.level.block.state.BlockState;
-
-public class FastRailBlock extends RailBlock {
-    public FastRailBlock(Properties properties) {
-        super(properties);
-    }
-
-    @Override
-    public float getRailMaxSpeed(BlockState state, Level world, BlockPos pos, AbstractMinecart cart) {
-        // Nastavíme stejný limit jako u tvého FastPoweredRailu (1.2f)
-        // Díky tomu vozík v zatáčce "necukne" a nezpomalí.
-        return 10F;
-    }
+public class FastRailBlock extends RailBlock{
+	public FastRailBlock(Properties properties){
+		super(properties);
+	}
+	@Override
+	public float getRailMaxSpeed(BlockState state,Level world,BlockPos pos,AbstractMinecart cart){
+		// Nastavíme stejný limit jako u tvého FastPoweredRailu (1.2f)
+		// Díky tomu vozík v zatáčce "necukne" a nezpomalí.
+		return 15F;
+	}
 }
