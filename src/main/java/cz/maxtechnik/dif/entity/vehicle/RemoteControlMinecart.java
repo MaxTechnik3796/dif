cc8c9528c74a6f9ad8919efd46282daa742f5d28
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-04-05 01:28:56 +0200

    RemoteControlMinecart (gamma)

--- src/main/java/cz/maxtechnik/dif/entity/vehicle/RemoteControlMinecart.java
+++ src/main/java/cz/maxtechnik/dif/entity/vehicle/RemoteControlMinecart.java
@@ -1,6 +1,5 @@
 package cz.maxtechnik.dif.entity.vehicle;
 
-import cz.maxtechnik.dif.DifMod;
 import cz.maxtechnik.dif.init.basic.DifModItems;
 import cz.maxtechnik.dif.init.other.DifModEntities;
 import net.minecraft.nbt.CompoundTag;
@@ -17,91 +16,100 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-
-import static java.lang.Math.round;
-public class RemoteControlMinecart extends AbstractMinecart{
+public class RemoteControlMinecart extends AbstractMinecart {
 	private double push;
-	public RemoteControlMinecart(EntityType<? extends AbstractMinecart> type,Level level){
-		super(type,level);
+	// Tyto proměnné si pamatují směr "motoru" nezávisle na rotaci entity
+	private double xPush;
+	private double zPush;
+
+	public RemoteControlMinecart(EntityType<? extends AbstractMinecart> type, Level level) {
+		super(type, level);
 	}
-	public RemoteControlMinecart(Level level,double x,double y,double z){
-		super(DifModEntities.REMOTE_MINECART.get(),level,x,y,z);
+
+	public RemoteControlMinecart(Level level, double x, double y, double z) {
+		super(DifModEntities.REMOTE_MINECART.get(), level, x, y, z);
 	}
+
 	@Override
-	public @NotNull Item getDropItem(){
+	public @NotNull Item getDropItem() {
 		return DifModItems.REMOTE_MINECART_ITEM.get();
 	}
-	// Definujeme, jaký blok se má zobrazovat uvnitř (např. tvůj Chunk Loader)
+
 	@Override
-	public @NotNull BlockState getDefaultDisplayBlockState(){
-		// NORTH je v Minecraftu pro Minecarty "předek"
+	public @NotNull BlockState getDefaultDisplayBlockState() {
+		// Orientace pece (NORTH = předek minecartu)
 		return net.minecraft.world.level.block.Blocks.BLAST_FURNACE.defaultBlockState()
-				.setValue(net.minecraft.world.level.block.BlastFurnaceBlock.FACING,net.minecraft.core.Direction.NORTH);
+				.setValue(net.minecraft.world.level.block.BlastFurnaceBlock.FACING, net.minecraft.core.Direction.NORTH);
 	}
+
 	@Override
-	public @NotNull Type getMinecartType(){
-		return Type.RIDEABLE; // Nebo si vytvoř vlastní typ
+	public @NotNull Type getMinecartType() {
+		return Type.RIDEABLE;
 	}
-	// Metoda, kterou budeme volat z dálkového ovladače (přes Packet)
-	public void setRemoteMovement(double push){
-		this.push=push;
+
+	public void setRemoteMovement(double pushValue) {
+		this.push = pushValue;
 	}
+
 	@Override
-	public void tick(){
+	public void tick() {
 		super.tick();
-		if(!this.level().isClientSide){
-			Vec3 currentMotion=this.getDeltaMovement();
-			// 1. PASIVNÍ BRŽDĚNÍ (Důležité pro pocit kontroly)
-			if(currentMotion.horizontalDistance()>0.001)this.setDeltaMovement(currentMotion.multiply(0.85,1,0.85));
-			float yaw=this.getYRot();
-			//DifMod.LOGGER.debug(String.valueOf(push));
-			switch(round(yaw)){
-				case 0->this.setDeltaMovement(-push,currentMotion.y,currentMotion.z);
-				case 90->this.setDeltaMovement(currentMotion.x,currentMotion.y,-push);
-				case 180->this.setDeltaMovement(push,currentMotion.y,currentMotion.z);
-				case -90,270->this.setDeltaMovement(currentMotion.x,currentMotion.y,push);
-			}
-			push*=0.1;
-			// 2. AKTIVNÍ POHON (Plynový pedál)
-			// remotePushX bereme jako plyn (1.0 = vpřed, -1.0 = vzad)
-			/*if (Math.abs(remotePushX) > 0.01) {
-				// Získáme čistý směr, kterým Minecart kouká
-				float yaw = this.getYRot();
-				Vec3 lookDir = Vec3.directionFromRotation(0, yaw);
 
-				// Cílová rychlost, kterou chceme mít (např. max 0.4 bloků/tick)
-				double targetSpeed = remotePushX * 0.4;
+		if (!this.level().isClientSide) {
+			Vec3 motion = this.getDeltaMovement();
+			double horizontalSpeed = Math.sqrt(xPush * xPush + zPush * zPush);
 
-				// TADY JE TA ZMĚNA:
-				// Místo .add() použijeme postupné přibližování k cílové rychlosti (Lerp)
-				double newX = currentMotion.x + (lookDir.x * targetSpeed - currentMotion.x) * 0.3;
-				double newZ = currentMotion.z + (lookDir.z * targetSpeed - currentMotion.z) * 0.3;
+			// 1. OVLÁDÁNÍ (Změna směru tlačení)
+			if (Math.abs(push) > 0.01) {
+				// Získáme směr podle aktuální rotace "nosu"
+				float yawRad = this.getYRot() * ((float) Math.PI / 180F);
+				double dirX = -Math.sin(yawRad);
+				double dirZ = Math.cos(yawRad);
 
-				this.setDeltaMovement(newX, currentMotion.y, newZ);
+				// "Nabijeme" motor směrem, kterým koukáme (push 1.0 = vpřed, -1.0 = vzad)
+				this.xPush = dirX * push;
+				this.zPush = dirZ * push;
 			}
 
-			// 3. VYČIŠTĚNÍ IMPULSU
-			// Aby se plyn neopakoval, pokud nepřijde další packet
-			remotePushX *= 0.1;*/
+			// 2. APLIKACE SÍLY (Logika FurnaceMinecart)
+			if (horizontalSpeed > 0.01) {
+				// Aplikujeme sílu: 80% starého pohybu + impuls motoru
+				// 0.1 je síla motoru, 0.8 je tření při zapnutém motoru
+				this.setDeltaMovement(motion.x * 0.8 + xPush * 0.1, motion.y, motion.z * 0.8 + zPush * 0.1);
+			} else {
+				// 3. PASIVNÍ BRŽDĚNÍ (Když motor netlačí)
+				if (motion.horizontalDistance() > 0.001) {
+					this.setDeltaMovement(motion.multiply(0.7, 1, 0.7));
+				} else {
+					this.setDeltaMovement(0, motion.y, 0);
+				}
+			}
+
+			// Plynulé vyhasínání motoru (aby Minecart nejel věčně)
+			xPush *= 0.9;
+			zPush *= 0.9;
+
+			// Resetování impulsu z ovladače pro tento tick
+			push = 0;
 		}
 	}
+
 	@Override
-	public @NotNull ItemStack getPickResult(){
+	public @NotNull ItemStack getPickResult() {
 		return new ItemStack(DifModItems.REMOTE_MINECART_ITEM.get());
 	}
+
 	@Override
-	public @NotNull InteractionResult interact(Player player,@NotNull InteractionHand hand){
-		ItemStack stack=player.getItemInHand(hand);
-		if(stack.is(DifModItems.REMOTE_CONTROLLER.get())){
-			if(!player.level().isClientSide){
-				// Uložíme UUID Minecartu do NBT ovladače
-				CompoundTag nbt=stack.getOrCreateTag();
-				nbt.putUUID("LinkedCart",this.getUUID());
-				player.displayClientMessage(Component.literal("Minecart linked!"),true);
+	public @NotNull InteractionResult interact(Player player, @NotNull InteractionHand hand) {
+		ItemStack stack = player.getItemInHand(hand);
+		if (stack.is(DifModItems.REMOTE_CONTROLLER.get())) {
+			if (!player.level().isClientSide) {
+				CompoundTag nbt = stack.getOrCreateTag();
+				nbt.putUUID("LinkedCart", this.getUUID());
+				player.displayClientMessage(Component.literal("Minecart linked!"), true);
 			}
 			return InteractionResult.SUCCESS;
 		}
-		return super.interact(player,hand);
+		return super.interact(player, hand);
 	}
 }
