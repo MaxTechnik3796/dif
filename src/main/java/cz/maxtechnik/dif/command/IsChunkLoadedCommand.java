3f3513493cd913af94b5a09d2f717a56a577f6ca
surik <suriken222a@gmail.com>
2026-04-05 14:26:48 +0200

    new command rework

--- src/main/java/cz/maxtechnik/dif/command/IsChunkLoadedCommand.java
+++ src/main/java/cz/maxtechnik/dif/command/IsChunkLoadedCommand.java
@@ -1,34 +1,31 @@
 package cz.maxtechnik.dif.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 
 public class IsChunkLoadedCommand {
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("ischunkloaded")
-            .executes(context -> {
-                CommandSourceStack source = context.getSource();
-                ServerLevel level = source.getLevel();
-                ChunkPos chunkPos = new ChunkPos(BlockPos.containing(source.getPosition()));
-                boolean isForced = level.getForcedChunks().contains(chunkPos.toLong());
-                if (isForced) {
-                    source.sendSuccess(() -> Component.literal("This chunk ")
-                        .append(Component.literal("[" + chunkPos.x + ", " + chunkPos.z + "]").withStyle(ChatFormatting.AQUA))
-                        .append(Component.literal(" is permanently loaded.").withStyle(ChatFormatting.GREEN)), false);
-                } else {
-                    source.sendFailure(Component.literal("This chunk ")
-                        .append(Component.literal("[" + chunkPos.x + ", " + chunkPos.z + "]").withStyle(ChatFormatting.AQUA))
-                        .append(Component.literal(" isn't permanently loaded.")));
-                }
-                return 1;
-            })
+                .executes(ctx -> {
+                    CommandSourceStack source = ctx.getSource();
+                    ChunkPos cp = new ChunkPos(BlockPos.containing(source.getPosition()));
+                    boolean forced = isForced(source.getLevel(), cp);
+                    source.sendSuccess(() -> Component.literal(forced ? "§aChunk " : "§cChunk ")
+                            .append(Component.literal("[" + cp.x + ", " + cp.z + "]").withStyle(ChatFormatting.AQUA))
+                            .append(forced ? " §ais permanently loaded." : " §cisn't permanently loaded."), false);
+
+                    return 1;
+                })
         );
+    }
+    public static boolean isForced(ServerLevel level, ChunkPos pos) {
+        return level.getForcedChunks().contains(pos.toLong());
     }
 }
