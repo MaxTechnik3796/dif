d3cc6f1fa58eb6fa4fa1bf89e0b3f1f676192af4
surik <suriken222a@gmail.com>
2026-04-09 18:21:55 +0200

    formule 2.2 key mapping

--- src/main/java/cz/maxtechnik/dif/client/CarInputHandler.java
+++ src/main/java/cz/maxtechnik/dif/client/CarInputHandler.java
@@ -2,42 +2,53 @@ package cz.maxtechnik.dif.client;
 
 import cz.maxtechnik.dif.DifMod;
 import cz.maxtechnik.dif.entity.vehicle.BaseCarEntity;
+import cz.maxtechnik.dif.init.other.DifModKeys;
 import cz.maxtechnik.dif.network.ShiftGearPacket;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.lwjgl.glfw.GLFW;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = DifMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CarInputHandler {
 
     @SubscribeEvent
     public static void onKeyInput(InputEvent.Key event) {
         Minecraft mc = Minecraft.getInstance();
+
+        // Základní kontroly: hráč musí existovat, nesmí být v menu a musí sedět v autě
         if (mc.player == null || mc.screen != null) return;
         if (!(mc.player.getVehicle() instanceof BaseCarEntity car)) return;
+
+        // Reagujeme pouze na stisk (Action 1), ne na držení nebo puštění[cite: 16]
         if (event.getAction() != 1) return;
 
-        int currentGear = car.getCurrentGear();
-        int maxGear = car.getGearRatios().length;
+        // Propojení na KeyMappingy z DifModKeys místo GLFW konstant
+        if (DifModKeys.GEAR_UP.consumeClick()) {
+            handleShift(car, 1);
+        } else if (DifModKeys.GEAR_DOWN.consumeClick()) {
+            handleShift(car, -1);
+        }
+    }
 
-        if (event.getKey() == GLFW.GLFW_KEY_R) { // GEAR UP
-            if (currentGear < maxGear) {
-                car.setCurrentGear(currentGear + 1);
-                sendPacket(+1);
-            }
-        } else if (event.getKey() == GLFW.GLFW_KEY_F) { // GEAR DOWN
-            if (currentGear > 1) {
-                car.setCurrentGear(currentGear - 1);
-                sendPacket(-1);
-            }
+    private static void handleShift(BaseCarEntity car, int direction) {
+        int currentGear = car.getCurrentGear(); //[cite: 16]
+        int maxGear = car.getGearRatios().length; //[cite: 16]
+
+        if (direction == 1 && currentGear < maxGear) {
+            // Logika pro přeřazení nahoru[cite: 16]
+            car.setCurrentGear(currentGear + 1);
+            sendPacket(1);
+        } else if (direction == -1 && currentGear > 1) {
+            // Logika pro přeřazení dolů[cite: 16]
+            car.setCurrentGear(currentGear - 1);
+            sendPacket(-1);
         }
     }
 
     private static void sendPacket(int direction) {
-        // Použijeme tvůj vlastní PACKET_HANDLER z DifMod.java
+        // Odeslání packetu přes tvůj PACKET_HANDLER[cite: 16]
         DifMod.PACKET_HANDLER.sendToServer(new ShiftGearPacket(direction));
     }
 }
--- src/main/java/cz/maxtechnik/dif/init/other/DifModKeys.java
+++ src/main/java/cz/maxtechnik/dif/init/other/DifModKeys.java
@@ -1,58 +1,81 @@
 package cz.maxtechnik.dif.init.other;
 
-import cz.maxtechnik.dif.DifMod;
 import com.mojang.blaze3d.platform.InputConstants;
-import cz.maxtechnik.dif.network.JetpackFlyMessage;
-import cz.maxtechnik.dif.network.EnderOpenMessage;
-import cz.maxtechnik.dif.network.RemoteControlPacket;
+import cz.maxtechnik.dif.DifMod;
+import cz.maxtechnik.dif.entity.vehicle.BaseCarEntity;
 import cz.maxtechnik.dif.init.basic.DifModItems;
+import cz.maxtechnik.dif.network.EnderOpenMessage;
+import cz.maxtechnik.dif.network.JetpackFlyMessage;
+import cz.maxtechnik.dif.network.RemoteControlPacket;
+import cz.maxtechnik.dif.network.ShiftGearPacket;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
-@Mod.EventBusSubscriber(modid=DifMod.MODID, bus=Mod.EventBusSubscriber.Bus.MOD, value=Dist.CLIENT)
-public class DifModKeys{
-	public static final String CATEGORY="key.categories.dif";
-	public static final KeyMapping JETPACK_FLY=new KeyMapping("key.dif.jetpack_fly",InputConstants.Type.KEYSYM,GLFW.GLFW_KEY_SPACE,CATEGORY);
-	public static final KeyMapping OPEN_ENDER_CHEST=new KeyMapping("key.dif.open_ender",InputConstants.Type.KEYSYM,GLFW.GLFW_KEY_V,CATEGORY);
-	// NOVÉ KLÁVESY PRO MINECART
-	public static final KeyMapping MINE_FORWARD=new KeyMapping("key.dif.remote_minecart_boost",InputConstants.Type.KEYSYM,GLFW.GLFW_KEY_I,CATEGORY);
-	public static final KeyMapping MINE_BACKWARD=new KeyMapping("key.dif.remote_minecart_invert",InputConstants.Type.KEYSYM,GLFW.GLFW_KEY_K,CATEGORY);
+
+@Mod.EventBusSubscriber(modid = DifMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class DifModKeys {
+	public static final String CATEGORY = "key.categories.dif";
+
+	// Původní klávesy
+	public static final KeyMapping JETPACK_FLY = new KeyMapping("key.dif.jetpack_fly", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_SPACE, CATEGORY);
+	public static final KeyMapping OPEN_ENDER_CHEST = new KeyMapping("key.dif.open_ender", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_V, CATEGORY);
+
+	// Původní Minecart ovladač
+	public static final KeyMapping MINE_FORWARD = new KeyMapping("key.dif.remote_minecart_boost", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_I, CATEGORY);
+	public static final KeyMapping MINE_BACKWARD = new KeyMapping("key.dif.remote_minecart_invert", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_K, CATEGORY);
+
+	// NOVÉ KLÁVESY PRO AUTO
+	public static final KeyMapping GEAR_UP = new KeyMapping("key.dif.gear_up", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R, CATEGORY);
+	public static final KeyMapping GEAR_DOWN = new KeyMapping("key.dif.gear_down", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_F, CATEGORY);
+
 	@SubscribeEvent
-	public static void registerKeys(RegisterKeyMappingsEvent event){
+	public static void registerKeys(RegisterKeyMappingsEvent event) {
 		event.register(JETPACK_FLY);
 		event.register(OPEN_ENDER_CHEST);
 		event.register(MINE_FORWARD);
 		event.register(MINE_BACKWARD);
+		event.register(GEAR_UP);
+		event.register(GEAR_DOWN);
 	}
-	@Mod.EventBusSubscriber(modid=DifMod.MODID, value=Dist.CLIENT, bus=Mod.EventBusSubscriber.Bus.FORGE)
-	public static class ClientTickHandler{
+
+	@Mod.EventBusSubscriber(modid = DifMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
+	public static class ClientTickHandler {
 		@SubscribeEvent
-		public static void onClientTick(TickEvent.ClientTickEvent event){
-			if(event.phase==TickEvent.Phase.END&&Minecraft.getInstance().player!=null){
-				var player=Minecraft.getInstance().player;
-				// Existující logika Jetpacku a Ender chesty
-				if(JETPACK_FLY.isDown()){
-					DifMod.PACKET_HANDLER.sendToServer(new JetpackFlyMessage(0,0));
-					JetpackFlyMessage.pressAction(player,0);
+		public static void onClientTick(TickEvent.ClientTickEvent event) {
+			if (event.phase == TickEvent.Phase.END && Minecraft.getInstance().player != null) {
+				var player = Minecraft.getInstance().player;
+
+				// 1. Logika řazení (pokud hráč sedí v autě)
+				if (player.getVehicle() instanceof BaseCarEntity) {
+					while (GEAR_UP.consumeClick()) {
+						DifMod.PACKET_HANDLER.sendToServer(new ShiftGearPacket(+1));
+					}
+					while (GEAR_DOWN.consumeClick()) {
+						DifMod.PACKET_HANDLER.sendToServer(new ShiftGearPacket(-1));
+					}
 				}
-				while(OPEN_ENDER_CHEST.consumeClick()){
-					DifMod.PACKET_HANDLER.sendToServer(new EnderOpenMessage(0,0));
+
+				// 2. Logika Jetpacku
+				if (JETPACK_FLY.isDown()) {
+					DifMod.PACKET_HANDLER.sendToServer(new JetpackFlyMessage(0, 0));
+					JetpackFlyMessage.pressAction(player, 0);
 				}
-				// NOVÁ LOGIKA: Ovládání Minecartu jako vozidla
-				// V ClientTickHandler v DifModKeys.java:
+
+				// 3. Logika Ender Chesty
+				while (OPEN_ENDER_CHEST.consumeClick()) {
+					DifMod.PACKET_HANDLER.sendToServer(new EnderOpenMessage(0, 0));
+				}
+
+				// 4. Logika dálkového ovládání Minecartu
 				if (player.getMainHandItem().is(DifModItems.REMOTE_CONTROLLER.get())) {
-					// PLYN (I) - Posíláme 1.0 dokud se drží
 					if (MINE_FORWARD.isDown()) {
 						DifMod.PACKET_HANDLER.sendToServer(new RemoteControlPacket(1.0, false));
 					}
-
-					// REVERZ (K) - Pošle signál k otočení jen při stisku
 					if (MINE_BACKWARD.consumeClick()) {
 						DifMod.PACKET_HANDLER.sendToServer(new RemoteControlPacket(0.0, true));
 					}
