5580bc4a059a1ef876232b357dcf47e41ba42387
surik <suriken222a@gmail.com>
2026-04-12 12:31:09 +0200

    formula 3.2 postava fix

---
+++ src/main/java/cz/maxtechnik/dif/renderer/TestScale.java
@@ -0,0 +1,7 @@
+package cz.maxtechnik.dif.renderer;
+import net.minecraft.client.model.geom.ModelPart;
+public class TestScale {
+    public void test(ModelPart part) {
+        part.xScale = 1.0f; part.yScale = 1.0f; part.zScale = 1.0f;
+    }
+}
