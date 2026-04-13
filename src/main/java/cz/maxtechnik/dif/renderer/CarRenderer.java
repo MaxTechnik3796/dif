f68370cd1c9c24b4ba729cd8ffb300b6867c251a
surik <suriken222a@gmail.com>
2026-04-13 19:17:54 +0200

    formula 3.4 oprava hitbox atd.

--- src/main/java/cz/maxtechnik/dif/renderer/CarRenderer.java
+++ src/main/java/cz/maxtechnik/dif/renderer/CarRenderer.java
@@ -3,7 +3,9 @@ package cz.maxtechnik.dif.renderer;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
+import cz.maxtechnik.dif.client.enitity.FormulaModel;
 import cz.maxtechnik.dif.entity.vehicle.BaseCarEntity;
+import cz.maxtechnik.dif.entity.vehicle.FormulaEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -17,51 +19,41 @@ import org.jetbrains.annotations.NotNull;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT)
 public class CarRenderer<T extends BaseCarEntity> extends EntityRenderer<T> {
-    private final cz.maxtechnik.dif.client.enitity.FormulaModel<T> model;
+    private final FormulaModel<T> model;
     private static final ResourceLocation TEX_BASE = ResourceLocation.fromNamespaceAndPath("dif", "textures/entity/f1_base.png");
     private static final ResourceLocation TEX_COLOR = ResourceLocation.fromNamespaceAndPath("dif", "textures/entity/f1_collored.png");
 
     public CarRenderer(EntityRendererProvider.Context ctx) {
-        super(ctx); 
-        this.shadowRadius = 0.7F; 
-        this.model = new cz.maxtechnik.dif.client.enitity.FormulaModel<>(ctx.bakeLayer(cz.maxtechnik.dif.client.enitity.FormulaModel.LAYER_LOCATION));
+        super(ctx);
+        this.shadowRadius = 0.7F;
+        this.model = new FormulaModel<>(ctx.bakeLayer(FormulaModel.LAYER_LOCATION));
     }
 
-    @Override 
+    @Override
     public void render(@NotNull T e, float y, float pt, PoseStack ps, MultiBufferSource b, int l) {
-        ps.pushPose(); 
-        
-        // --- POZICOVÁNÍ MODELU (Zde můžeš upravit) ---
-        // Zde můžeš upravit rotaci a pozici modelu
-        // Rotace auta: otáčí se podle Y rotace (yaw)
-        ps.mulPose(Axis.YP.rotationDegrees(180F - y)); 
-        // Posun modelu nahoru nebo dolů (např. 1.5D je blok a půl)
-        // Můžeš upravit střední hodnotu (0.5D) např na 1.5D, abys ho posunul nahoru či dolů
-        ps.translate(0D, 1.5D, 0D); 
-        // Obracecí škálovaní (Blockbench exportuje vzhůru nohama)
-        ps.scale(-1.0F, -1.0F, 1.0F);
-        // ---------------------------------------------
-        
-        // Vypočítáme rotaci kol podle rychlosti (simulace pohybu)
-        float wheelRotation = (e.tickCount + pt) * e.getSpeedKmh() * 0.02f;
-        this.model.setupAnim(e, wheelRotation, 0.0F, e.tickCount + pt, e.getYRot(), e.getXRot());
-        
-        // 1. ZÁKLADNÍ TEXTURA (vždy se renderuje)
-        VertexConsumer vertexConsumer = b.getBuffer(this.model.renderType(TEX_BASE));
-        this.model.renderToBuffer(ps, vertexConsumer, l, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        ps.pushPose();
+        // Rotace modelu podle yaw
+        ps.mulPose(Axis.YP.rotationDegrees(180F - y));
+        // Pozice modelu (uprav Y hodnotu pro výšku)
+        ps.translate(0D, 1.5D, 0D);
+        // Flip (Blockbench export je vzhůru nohama)
+        ps.scale(-1F, -1F, 1F);
 
-        // 2. BAREVNÁ TEXTURA (pouze pokud je auto obarvené tintou)
-        if (e instanceof cz.maxtechnik.dif.entity.vehicle.FormulaEntity formula && formula.getColor() != -1) {
-            int color = formula.getColor();
-            float r = (float)(color >> 16 & 255) / 255.0F;
-            float g = (float)(color >> 8 & 255) / 255.0F;
-            float blue = (float)(color & 255) / 255.0F;
-            
-            VertexConsumer colorConsumer = b.getBuffer(this.model.renderType(TEX_COLOR));
-            this.model.renderToBuffer(ps, colorConsumer, l, OverlayTexture.NO_OVERLAY, r, g, blue, 1.0F);
+        // Animace kol – rotace podle rychlosti
+        float wheelSpin = (e.tickCount + pt) * e.getSpeedKmh() * 0.02f;
+        model.setupAnim(e, wheelSpin, 0F, e.tickCount + pt, 0F, 0F);
+
+        // Base textura (vždy)
+        model.renderToBuffer(ps, b.getBuffer(model.renderType(TEX_BASE)), l, OverlayTexture.NO_OVERLAY, 1F, 1F, 1F, 1F);
+
+        // Barevná vrstva (pokud obarvená)
+        if (e instanceof FormulaEntity f) {
+            int c = f.getColor();
+            float r = (c >> 16 & 255) / 255F, g = (c >> 8 & 255) / 255F, bl = (c & 255) / 255F;
+            model.renderToBuffer(ps, b.getBuffer(model.renderType(TEX_COLOR)), l, OverlayTexture.NO_OVERLAY, r, g, bl, 1F);
         }
 
-        ps.popPose(); 
+        ps.popPose();
         super.render(e, y, pt, ps, b, l);
     }
 
@@ -70,23 +62,17 @@ public class CarRenderer<T extends BaseCarEntity> extends EntityRenderer<T> {
     @SubscribeEvent
     public static void onPlayerRenderPre(RenderPlayerEvent.Pre event) {
         if (event.getEntity().getVehicle() instanceof BaseCarEntity car) {
-            float pt = event.getPartialTick();
-            float lerpYaw = net.minecraft.util.Mth.lerp(pt, car.yRotO, car.getYRot());
-            event.getEntity().yBodyRot = lerpYaw;
-            event.getEntity().yBodyRotO = lerpYaw;
-            event.getRenderer().getModel().rightLeg.yScale = 0.5F;
-            event.getRenderer().getModel().leftLeg.yScale = 0.5F;
-            
-            event.getRenderer().getModel().rightPants.yScale = 0.5F;
-            event.getRenderer().getModel().leftPants.yScale = 0.5F;
+            float yaw = net.minecraft.util.Mth.lerp(event.getPartialTick(), car.yRotO, car.getYRot());
+            event.getEntity().yBodyRot = yaw;
+            event.getEntity().yBodyRotO = yaw;
+            var m = event.getRenderer().getModel();
+            m.rightLeg.yScale = m.leftLeg.yScale = m.rightPants.yScale = m.leftPants.yScale = 0.5F;
         }
     }
 
     @SubscribeEvent
     public static void onPlayerRenderPost(RenderPlayerEvent.Post event) {
-        event.getRenderer().getModel().rightLeg.yScale = 1.0F;
-        event.getRenderer().getModel().leftLeg.yScale = 1.0F;
-        event.getRenderer().getModel().rightPants.yScale = 1.0F;
-        event.getRenderer().getModel().leftPants.yScale = 1.0F;
+        var m = event.getRenderer().getModel();
+        m.rightLeg.yScale = m.leftLeg.yScale = m.rightPants.yScale = m.leftPants.yScale = 1.0F;
     }
 }
