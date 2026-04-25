129c453dc6012a09752368c7ce83e843b4de5d5d
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-04-25 20:59:22 +0200

    quarry frame progress (2)

--- src/main/java/cz/maxtechnik/dif/block/QuarryFrame.java
+++ src/main/java/cz/maxtechnik/dif/block/QuarryFrame.java
@@ -1,6 +1,5 @@
 package cz.maxtechnik.dif.block;
 
-import cz.maxtechnik.dif.DifMod;
 import cz.maxtechnik.dif.block.entity.QuarryFrameBlockEntity;
 import cz.maxtechnik.dif.init.basic.DifModBlocks;
 import cz.maxtechnik.dif.init.other.DifModBlockEntities;
@@ -8,6 +7,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -59,30 +59,23 @@ public class QuarryFrame extends BaseEntityBlock{
 		return Shapes.empty();
 	}
 	@Override
-	public boolean skipRendering(@NotNull BlockState state,BlockState adjacentBlockState,@NotNull Direction side) {
+	public boolean skipRendering(@NotNull BlockState state,BlockState adjacentBlockState,@NotNull Direction side){
 		return adjacentBlockState.getBlock()==this||super.skipRendering(state,adjacentBlockState,side);
 	}
-
 	@Override
 	public int getLightBlock(@NotNull BlockState state,@NotNull BlockGetter worldIn,@NotNull BlockPos pos){
 		return 0;
 	}
 	@Override
-	public void neighborChanged(@NotNull BlockState blockState,@NotNull Level world,@NotNull BlockPos pos,@NotNull Block neighborBlock,@NotNull BlockPos fromPos,boolean moving){
-		super.neighborChanged(blockState,world,pos,neighborBlock,fromPos,moving);
-		BlockState northState=world.getBlockState(pos.relative(Direction.NORTH));
-		BlockState eastState=world.getBlockState(pos.relative(Direction.EAST));
-		BlockState southState=world.getBlockState(pos.relative(Direction.SOUTH));
-		BlockState westState=world.getBlockState(pos.relative(Direction.WEST));
-		BlockState upState=world.getBlockState(pos.relative(Direction.UP));
-		BlockState downState=world.getBlockState(pos.relative(Direction.DOWN));
-
-		blockState.setValue(NORTH,(northState.is(this)||northState.is(DifModBlocks.QUARRY.get())));
-		blockState.setValue(EAST,(eastState.is(this)||eastState.is(DifModBlocks.QUARRY.get())));
-		blockState.setValue(SOUTH,(southState.is(this)||southState.is(DifModBlocks.QUARRY.get())));
-		blockState.setValue(WEST,(westState.is(this)||westState.is(DifModBlocks.QUARRY.get())));
-		blockState.setValue(UP,(upState.is(this)||upState.is(DifModBlocks.QUARRY.get())));
-		blockState.setValue(DOWN,(downState.is(this)||downState.is(DifModBlocks.QUARRY.get())));
-		world.setBlock(pos,blockState,3);
+	public @NotNull BlockState updateShape(@NotNull BlockState state,Direction direction,BlockState neighborState,@NotNull LevelAccessor world,@NotNull BlockPos pos,@NotNull BlockPos neighborPos){
+		boolean shouldConnect=neighborState.is(this)||neighborState.is(DifModBlocks.QUARRY.get());
+		return switch(direction){
+			case NORTH -> state.setValue(NORTH,shouldConnect);
+			case SOUTH -> state.setValue(SOUTH,shouldConnect);
+			case WEST -> state.setValue(WEST,shouldConnect);
+			case EAST -> state.setValue(EAST,shouldConnect);
+			case UP -> state.setValue(UP,shouldConnect);
+			case DOWN -> state.setValue(DOWN,shouldConnect);
+		};
 	}
 }
