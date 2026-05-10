9ce06de33efb4c478e8881261a09a54b39cdd760
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-05-10 18:35:44 +0200

    composter fix

--- src/main/java/cz/maxtechnik/dif/init/events/ComposterFurnaceInjector.java
+++ src/main/java/cz/maxtechnik/dif/init/events/ComposterFurnaceInjector.java
@@ -10,20 +10,9 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.event.furnace.FurnaceFuelBurnTimeEvent;
-
 @SuppressWarnings("removal")
-@EventBusSubscriber(modid = DifMod.MODID, bus = EventBusSubscriber.Bus.GAME)
+@EventBusSubscriber(modid=DifMod.MODID, bus=EventBusSubscriber.Bus.GAME)
 public class ComposterFurnaceInjector{
-	@SubscribeEvent
-	public static void addComposterItems(FMLCommonSetupEvent event){
-		ComposterBlock.COMPOSTABLES.put(Blocks.BAMBOO.asItem(),0.4F);
-		ComposterBlock.COMPOSTABLES.put(DifModItems.MATA.get(),0.9F);
-		ComposterBlock.COMPOSTABLES.put(DifModItems.MATA_PLANT.get(),0.8F);
-		ComposterBlock.COMPOSTABLES.put(DifModItems.MATY_BLOCK.get(),1F);
-		ComposterBlock.COMPOSTABLES.put(DifModItems.CANOLA_PLANT.get(),1F);
-		ComposterBlock.COMPOSTABLES.put(DifModItems.CANOLA_SEEDS.get(),1F);
-		ComposterBlock.COMPOSTABLES.put(DifModItems.CHERRY.get(),0.6F);
-	}
 	@SubscribeEvent
 	public static void furnaceFuelBurnTimeEvent(FurnaceFuelBurnTimeEvent event){
 		ItemStack itemstack=event.getItemStack();
