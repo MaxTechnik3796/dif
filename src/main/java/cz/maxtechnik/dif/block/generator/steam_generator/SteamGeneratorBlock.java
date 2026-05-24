c4574cb6e49e3dd7ca1120f0b8209475ede69787
surik <suriken222a@gmail.com>
2026-05-24 20:08:47 +0200

    78 (ono sa to točí :D)

--- src/main/java/cz/maxtechnik/dif/block/generator/steam_generator/SteamGeneratorBlock.java
+++ src/main/java/cz/maxtechnik/dif/block/generator/steam_generator/SteamGeneratorBlock.java
@@ -1,49 +1,64 @@
 package cz.maxtechnik.dif.block.generator.steam_generator;
 
 import com.simibubi.create.content.kinetics.base.KineticBlock;
+import cz.maxtechnik.dif.init.other.DifModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-public class SteamGeneratorBlock extends KineticBlock implements EntityBlock{
-	// Vlastnost pro natočení hřídele (osa X, Y nebo Z)
-	public static final EnumProperty<Direction.Axis> AXIS=BlockStateProperties.AXIS;
-	public SteamGeneratorBlock(Properties properties){
+
+public class SteamGeneratorBlock extends KineticBlock implements EntityBlock {
+
+	public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
+
+	public SteamGeneratorBlock(BlockBehaviour.Properties properties) {
 		super(properties);
-		// Výchozí osa bude svislá Y
-		this.registerDefaultState(this.stateDefinition.any().setValue(AXIS,Direction.Axis.Y));
+		this.registerDefaultState(this.stateDefinition.any().setValue(AXIS, Direction.Axis.Y));
 	}
-	// Vytvoří mozek bloku (BlockEntity)
+
 	@Override
-	public @Nullable BlockEntity newBlockEntity(@NotNull BlockPos pos,@NotNull BlockState state){
-		return new SteamGeneratorBlockEntity(pos,state);
+	public @Nullable BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
+		return new SteamGeneratorBlockEntity(pos, state);
 	}
-	// Řekne Create modu, v jaké ose se blok točí
+
 	@Override
-	public Direction.Axis getRotationAxis(BlockState state){
+	public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(@NotNull Level level, @NotNull BlockState state, @NotNull BlockEntityType<T> type) {
+		if (type != DifModBlockEntities.STEAM_GENERATOR.get()) return null;
+		return (lvl, pos, st, be) -> {
+			if (be instanceof SteamGeneratorBlockEntity kbe) kbe.tick();
+		};
+	}
+
+	@Override
+	public Direction.Axis getRotationAxis(BlockState state) {
 		return state.getValue(AXIS);
 	}
-	// Povolí připojení hřídelí pouze ze stran, kam směřuje osa
+
 	@Override
-	public boolean hasShaftTowards(LevelReader world,BlockPos pos,BlockState state,Direction face){
-		return face.getAxis()==getRotationAxis(state);
+	public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face) {
+		return face.getAxis() == getRotationAxis(state);
 	}
-	// Natočí blok podle toho, na jakou stranu bloku hráč kliknul při položení
+
 	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context){
-		return this.defaultBlockState().setValue(AXIS,context.getClickedFace().getAxis());
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return this.defaultBlockState().setValue(AXIS, context.getClickedFace().getAxis());
 	}
+
 	@Override
-	protected void createBlockStateDefinition(StateDefinition.Builder<Block,BlockState> builder){
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		builder.add(AXIS);
 	}
 }
--- src/main/java/cz/maxtechnik/dif/block/generator/steam_generator/SteamGeneratorBlockEntity.java
+++ src/main/java/cz/maxtechnik/dif/block/generator/steam_generator/SteamGeneratorBlockEntity.java
@@ -4,26 +4,28 @@ import com.simibubi.create.content.kinetics.base.GeneratingKineticBlockEntity;
 import cz.maxtechnik.dif.init.other.DifModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
-public class SteamGeneratorBlockEntity extends GeneratingKineticBlockEntity{
-	public SteamGeneratorBlockEntity(BlockPos pos,BlockState blockState){
-		super(DifModBlockEntities.STEAM_GENERATOR.get(),pos,blockState);
+
+public class SteamGeneratorBlockEntity extends GeneratingKineticBlockEntity {
+
+	public SteamGeneratorBlockEntity(BlockPos pos, BlockState blockState) {
+		super(DifModBlockEntities.STEAM_GENERATOR.get(), pos, blockState);
 	}
-	// Rychlost generování (96 RPM)
+
 	@Override
-	public float getGeneratedSpeed(){
+	public float getGeneratedSpeed() {
 		return 96.0F;
 	}
-	// Síla / Stress kapacita (512 SU)
+
 	@Override
-	public float calculateAddedStressCapacity(){
-		this.lastCapacityProvided=512.0F;
+	public float calculateAddedStressCapacity() {
+		this.lastCapacityProvided = 512.0F;
 		return this.lastCapacityProvided;
 	}
-	// Probudí Create síť hned po položení bloku do světa
+
 	@Override
-	public void initialize(){
+	public void initialize() {
 		super.initialize();
-		if(level!=null&&!level.isClientSide){
+		if (level != null && !level.isClientSide) {
 			updateGeneratedRotation();
 		}
 	}
