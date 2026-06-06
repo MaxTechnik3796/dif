73f498a0c8133d51070ae004efac93c3614afac1
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-06-06 18:46:47 +0200

    warn fix

--- src/main/java/cz/maxtechnik/dif/init/other/DifModDimensions.java
+++ src/main/java/cz/maxtechnik/dif/init/other/DifModDimensions.java
@@ -5,16 +5,15 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.dimension.DimensionType;
 public class DifModDimensions{
-	public static final ResourceKey<DimensionType> MOON_TYPE=ResourceKey.create(
+	/*public static final ResourceKey<DimensionType> MOON_TYPE=ResourceKey.create(
 			Registries.DIMENSION_TYPE,
 			ResourceLocation.fromNamespaceAndPath(DifMod.MODID,"moon_type")
 	);
 	public static final ResourceKey<DimensionType> ORBIT_TYPE=ResourceKey.create(
 			Registries.DIMENSION_TYPE,
 			ResourceLocation.fromNamespaceAndPath(DifMod.MODID,"orbit_type")
-	);
+	);*/
 	public static final ResourceKey<Level> MOON=ResourceKey.create(
 			Registries.DIMENSION,
 			ResourceLocation.fromNamespaceAndPath(DifMod.MODID,"moon")
