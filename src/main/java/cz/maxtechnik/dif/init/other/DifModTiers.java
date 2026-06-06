8b9d970dac644ac3c99769dab54afbf3ee315e96
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-06-06 19:08:46 +0200

    warn fix

--- src/main/java/cz/maxtechnik/dif/init/other/DifModTiers.java
+++ src/main/java/cz/maxtechnik/dif/init/other/DifModTiers.java
@@ -12,6 +12,8 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.EnumMap;
 import java.util.List;
 
@@ -24,13 +26,13 @@ public class DifModTiers{
         @Override
         public float getAttackDamageBonus(){return 2.0F;}
         @Override
-        public TagKey<Block> getIncorrectBlocksForDrops(){
+        public @NotNull TagKey<Block> getIncorrectBlocksForDrops(){
             return BlockTags.INCORRECT_FOR_STONE_TOOL;
         }
         @Override
         public int getEnchantmentValue(){return 13;}
         @Override
-        public Ingredient getRepairIngredient(){return Ingredient.of(Items.COPPER_INGOT);}
+        public @NotNull Ingredient getRepairIngredient(){return Ingredient.of(Items.COPPER_INGOT);}
     };
 
     @SuppressWarnings("unused")
