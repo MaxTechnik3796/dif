6ccdfeda299c6524ad67f195d10b1d6691415daa
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-06-21 22:34:09 +0200

    modular modifiers desc prep

--- src/main/java/cz/maxtechnik/dif/item/modular/v2/ModularModifier.java
+++ src/main/java/cz/maxtechnik/dif/item/modular/v2/ModularModifier.java
@@ -4,25 +4,27 @@ import java.util.Locale;
 
 public enum ModularModifier{
 	//material:
-	ECOLOGICAL("ecological",1F,1F,1F,1F,0x745631),
-	MAGNETIC("magnetic",1F,1F,1F,1F,0xDCDCDC),
-	CHEAP("cheap",1F,1F,1F,1F,0x838383),
-	SHINY("shiny",1F,1F,1F,1F,0xFFD700),
+	ECOLOGICAL("ecological",false,.1F,1F,1F,1F,0x745631),
+	MAGNETIC("magnetic",false,1F,1F,1F,1F,0xDCDCDC),
+	CHEAP("cheap",false,1F,1F,1F,1F,0x838383),
+	SHINY("shiny",false,1F,1F,1F,1F,0xFFD700),
 
 	//special:
-	EXCAVATOR("excavator",1F,1F,1F,1F,0xFFFFFF);
+	EXCAVATOR("excavator",true,1F,1F,1F,1F,0xFFFFFF);
 
 
 
 
 	private final String name;
+	private final boolean hasDescription;
 	private final float attackDamage;
 	private final float attackSpeed;
 	private final float efficiency;
 	private final float durability;
 	private final int color;
-	ModularModifier(String name,Float attackDamage,Float attackSpeed,Float efficiency,float durability, int color){
+	ModularModifier(String name,boolean hasDescription,Float attackDamage,Float attackSpeed,Float efficiency,float durability, int color){
 		this.name=name;
+		this.hasDescription=hasDescription;
 		this.attackSpeed=attackSpeed;
 		this.attackDamage=attackDamage;
 		this.efficiency=efficiency;
@@ -31,6 +33,9 @@ public enum ModularModifier{
 	}
 	public String getName(){
 		return name;
+	}
+	public boolean hasDescription(){
+		return hasDescription;
 	}
 	public float getAttackDamage(){
 		return attackDamage;
