b430731c8efe0b3cef322ec8bf2edb4f4dd4437b
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-06-21 21:58:11 +0200

    Revert "146"
    This reverts commit 3f171b64fd35359d3424d653b5c444db0847e057.

--- src/main/java/cz/maxtechnik/dif/item/modular/v2/ModularToolProperties.java
+++ src/main/java/cz/maxtechnik/dif/item/modular/v2/ModularToolProperties.java
@@ -2,14 +2,13 @@ package cz.maxtechnik.dif.item.modular.v2;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-public record ModularToolProperties(String toolType,String headMaterial,String bindingMaterial,String handleMaterial,String tier,float critMultiplier){
+public record ModularToolProperties(String toolType,String headMaterial,String bindingMaterial,String handleMaterial,String tier){
 	public static final Codec<ModularToolProperties> CODEC=RecordCodecBuilder.create(instance->instance.group(
 			Codec.STRING.fieldOf("tool_type").forGetter(ModularToolProperties::toolType),
 			Codec.STRING.fieldOf("head_material").forGetter(ModularToolProperties::headMaterial),
 			Codec.STRING.fieldOf("binding_material").forGetter(ModularToolProperties::bindingMaterial),
 			Codec.STRING.fieldOf("handle_material").forGetter(ModularToolProperties::handleMaterial),
-			Codec.STRING.fieldOf("tier").forGetter(ModularToolProperties::tier),
-			Codec.FLOAT.optionalFieldOf("crit_multiplier",1.0F).forGetter(ModularToolProperties::critMultiplier)
+			Codec.STRING.fieldOf("tier").forGetter(ModularToolProperties::tier)
 	).apply(instance,ModularToolProperties::new));
-	public static final ModularToolProperties DEFAULT=new ModularToolProperties("none","wood","wood","wood","common",1.0F);
+	public static final ModularToolProperties DEFAULT=new ModularToolProperties("none","wood","wood","wood","common");
 }
