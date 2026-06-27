d45966a5a7c10f66674cce0c2b4c0bbe37e90a79
surik <suriken222a@gmail.com>
2026-06-27 18:02:35 +0200

    151

--- src/main/java/cz/maxtechnik/dif/item/modular/v2/ModularSubscriber.java
+++ src/main/java/cz/maxtechnik/dif/item/modular/v2/ModularSubscriber.java
@@ -6,30 +6,33 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+
 @EventBusSubscriber
 public class ModularSubscriber{
 	@SubscribeEvent
-	public static void onEntityAttacked(LivingIncomingDamageEvent event){
+	public static void onEntityAttacked(LivingDamageEvent.Pre event){
 		if(!(event.getSource().getEntity() instanceof Player attacker)) return;
 		LivingEntity target=event.getEntity();
-		float dmg=event.getAmount();
+		float dmg=event.getNewDamage();
 		ItemStack tool=attacker.getMainHandItem();
 		if(!(tool.getItem() instanceof ModularTool)) return;
 		ModularToolProperties props=ModularTool.getProps(tool);
 		if(props==null) return;
 		ModularReforge reforge=ModularReforge.byName(props.reforge());
 		int tier=ModularTier.byName(props.tier()).getReforgeIndex();
-		int[] chances={0,0,9,6,4};
+		int[] chances={0,0,8,5,3};
 		switch(reforge){
 			case VAMPIRIC,DRAIN -> {
 				if(attacker.getHealth()>=attacker.getMaxHealth()) return;
-				if(!(chances[tier]==0)&&DifMod.rouletteBoolean(chances[tier]))
+				if(chances[tier]!=0&&DifMod.rouletteBoolean(chances[tier]))
 					attacker.heal(Math.clamp(dmg/2F,0.5F,target.getHealth()));
 			}
 			case PHANTOM -> {
-				if(!(chances[tier]==0)&&DifMod.rouletteBoolean(chances[tier]))
-					event.setAmount(dmg+Math.max(0.5F,dmg/2F));
+				if(chances[tier]!=0&&DifMod.rouletteBoolean(chances[tier])){
+					float newDmg = dmg + Math.max(0.5F, dmg/2F);
+					event.setNewDamage(newDmg);
+				}
 			}
 		}
 	}
