e5fd43d9dbf99b4c1896fcf9a989073ddb5f717a
surik <suriken222a@gmail.com>
2026-06-28 11:12:44 +0200

    154

--- src/main/java/cz/maxtechnik/dif/init/other/DifModRecipes.java
+++ src/main/java/cz/maxtechnik/dif/init/other/DifModRecipes.java
@@ -13,14 +13,9 @@ public class DifModRecipes{
 	public static final DeferredRegister<RecipeSerializer<?>> REGISTRY=DeferredRegister.create(Registries.RECIPE_SERIALIZER,DifMod.MODID);
 	public static final DeferredRegister<RecipeType<?>> TYPE_REGISTRY=DeferredRegister.create(Registries.RECIPE_TYPE,DifMod.MODID);
 	public static final DeferredHolder<RecipeSerializer<?>,FryingRecipe.Serializer> FRYING_SERIALIZER=REGISTRY.register("frying",FryingRecipe.Serializer::new);
-	public static final DeferredHolder<RecipeType<?>,RecipeType<FryingRecipe>> FRYING_TYPE=TYPE_REGISTRY.register("frying",()->FryingRecipe.Type.INSTANCE);
+	public static final DeferredHolder<RecipeType<?>,RecipeType<FryingRecipe>> FRYING_TYPE=TYPE_REGISTRY.register("frying",()->RecipeType.simple(ResourceLocation.fromNamespaceAndPath(DifMod.MODID,"frying")));
 	public static final DeferredHolder<RecipeSerializer<?>,DistillationRecipe.Serializer> DISTILLATION_SERIALIZER=REGISTRY.register("distillation",DistillationRecipe.Serializer::new);
-	public static final DeferredHolder<RecipeType<?>,RecipeType<DistillationRecipe>> DISTILLATION_TYPE=TYPE_REGISTRY.register("distillation",()->new RecipeType<>(){
-		@Override
-		public String toString(){
-			return DifMod.MODID+":distillation";
-		}
-	});
+	public static final DeferredHolder<RecipeType<?>,RecipeType<DistillationRecipe>> DISTILLATION_TYPE=TYPE_REGISTRY.register("distillation",()->RecipeType.simple(ResourceLocation.fromNamespaceAndPath(DifMod.MODID,"distillation")));
 	public static final DeferredHolder<RecipeSerializer<?>,RecipeSerializer<?>> MODULAR_REPAIR_SERIALIZER=REGISTRY.register("modular_recipe",ModularRecipes.Serializer::new);
 	public static final DeferredHolder<RecipeType<?>, RecipeType<CokeOvenRecipe>> COKE_OVEN_TYPE = TYPE_REGISTRY.register("coke_oven", () -> RecipeType.simple(ResourceLocation.fromNamespaceAndPath(DifMod.MODID,"coke_oven")));
 	public static final DeferredHolder<RecipeSerializer<?>, RecipeSerializer<CokeOvenRecipe>> COKE_OVEN_SERIALIZER = REGISTRY.register("coke_oven", CokeOvenRecipe.Serializer::new);
