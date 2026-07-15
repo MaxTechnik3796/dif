9e9c2e5f9a57ea3b8963d646c0c8d97d889056d7
surik <suriken222a@gmail.com>
2026-07-15 23:04:08 +0200

    238

--- src/main/java/cz/maxtechnik/dif/init/events/QuarryStats.java
+++ src/main/java/cz/maxtechnik/dif/init/events/QuarryStats.java
@@ -1,12 +1,12 @@
 package cz.maxtechnik.dif.init.events;
 
 public class QuarryStats{
-	public static final int IRON_ENGINE_QP_GEN=15;
-	public static final int IRON_ENGINE_FE_COST=50;
-	public static final int GOLD_ENGINE_QP_GEN=50;
-	public static final int GOLD_ENGINE_FE_COST=500;
-	public static final int DIAMOND_ENGINE_QP_GEN=40;
-	public static final int DIAMOND_ENGINE_FE_COST=200;
+	public static final int IRON_ENGINE_QP_GEN=20;
+	public static final int IRON_ENGINE_FE_COST=75;
+	public static final int GOLD_ENGINE_QP_GEN=30;
+	public static final int GOLD_ENGINE_FE_COST=200;
+	public static final int DIAMOND_ENGINE_QP_GEN=50;
+	public static final int DIAMOND_ENGINE_FE_COST=500;
 	public static final int QUARRY_ENERGY_CAPACITY=100000;
 	public static final int QUARRY_ENERGY_INPUT=5000;
 	public static final float MIN_PROGRESS_PER_TICK=0.5f;
