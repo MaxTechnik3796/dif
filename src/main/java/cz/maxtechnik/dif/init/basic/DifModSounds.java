7c4d366dbd0a9fe826b3511a001b2f06b78348e2
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-07-15 23:02:37 +0200

    sound

--- src/main/java/cz/maxtechnik/dif/init/basic/DifModSounds.java
+++ src/main/java/cz/maxtechnik/dif/init/basic/DifModSounds.java
@@ -7,6 +7,7 @@ import net.minecraft.sounds.SoundEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 public class DifModSounds{
-    public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(Registries.SOUND_EVENT, DifMod.MODID);
-    public static final DeferredHolder<SoundEvent,SoundEvent>FORMULA_ENGINE=REGISTRY.register("formula_engine",()->SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(DifMod.MODID, "formula_engine")));
+	public static final DeferredRegister<SoundEvent> REGISTRY=DeferredRegister.create(Registries.SOUND_EVENT,DifMod.MODID);
+	public static final DeferredHolder<SoundEvent,SoundEvent> FORMULA_ENGINE=REGISTRY.register("formula_engine",()->SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(DifMod.MODID,"formula_engine")));
+	public static final DeferredHolder<SoundEvent,SoundEvent> NANO_GLASS=REGISTRY.register("nano_glass",()->SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(DifMod.MODID,"nano_glass")));
 }
