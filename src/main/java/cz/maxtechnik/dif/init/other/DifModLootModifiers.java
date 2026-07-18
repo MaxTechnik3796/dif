d8bb7bbe63598318ff72b86c8d5254214cae096c
surik <suriken222a@gmail.com>
2026-07-18 10:43:33 +0200

    Revert "1.1.1-HOTFIX"
    This reverts commit b9652218342d018c981268736dd0d51a1e08856a.

--- src/main/java/cz/maxtechnik/dif/init/other/DifModLootModifiers.java
+++ src/main/java/cz/maxtechnik/dif/init/other/DifModLootModifiers.java
@@ -2,18 +2,14 @@ package cz.maxtechnik.dif.init.other;
 
 import com.mojang.serialization.MapCodec;
 import cz.maxtechnik.dif.DifMod;
-import cz.maxtechnik.dif.loot.AddItemModifier;
 import cz.maxtechnik.dif.loot.AddMeatLootModifier;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
-@SuppressWarnings("unused")
 public class DifModLootModifiers{
 	public static final DeferredRegister<MapCodec<? extends IGlobalLootModifier>> REGISTRY=
 			DeferredRegister.create(NeoForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS,DifMod.MODID);
 	public static final DeferredHolder<MapCodec<? extends IGlobalLootModifier>,MapCodec<AddMeatLootModifier>> ADD_MEAT=
 			REGISTRY.register("add_meat",()->AddMeatLootModifier.CODEC);
-	public static final DeferredHolder<MapCodec<? extends IGlobalLootModifier>,MapCodec<AddItemModifier>> ADD_ITEM=
-			REGISTRY.register("add_item",()->AddItemModifier.CODEC);
 }
