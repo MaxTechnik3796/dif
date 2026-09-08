820e982a5abd0638a0a3bd8d919fc4978ca1cfc1
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-09-08 21:09:47 +0200

    chicken?

--- src/main/java/cz/maxtechnik/dif/init/basic/DifModItems.java
+++ src/main/java/cz/maxtechnik/dif/init/basic/DifModItems.java
@@ -116,6 +116,7 @@ public class DifModItems{
 
 	public static final DeferredItem<Item> BUCKET_OF_CHICKEN=REGISTRY.register("bucket_of_chicken",()->new RetvalFoods(new Item.Properties().food(DifModFoods.BUCKET_OF_CHICKEN),Items.BUCKET,UseAnim.EAT));
 	public static final DeferredItem<Item> FRIES=REGISTRY.register("fries",()->new Item(new Item.Properties().food(DifModFoods.FRIES)));
+	//public static final DeferredItem<Item> FRIED_CHICKEN=REGISTRY.register("fried_chicken",()->new Item(new Item.Properties().food(Foods.COOKED_CHICKEN)));
 
 	public static final DeferredItem<Item> HORSE_MEAT=REGISTRY.register("horse_meat",()->new Item(new Item.Properties().food(DifModFoods.HORSE_MEAT)));
 	public static final DeferredItem<Item> COOKED_HORSE_MEAT=REGISTRY.register("cooked_horse_meat",()->new Item(new Item.Properties().food(DifModFoods.COOKED_HORSE_MEAT)));
